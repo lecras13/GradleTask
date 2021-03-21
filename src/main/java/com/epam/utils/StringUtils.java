a67d54faf3672f52255c544e0a1b7ad8df9f3180
lecras13 <lecras@tut.by>
2021-03-21 14:25:20 +0300

    after review

--- src/main/java/com/epam/utils/StringUtils.java
+++ src/main/java/com/epam/utils/StringUtils.java
@@ -1,7 +1,17 @@
 package com.epam.utils;
 
+import org.apache.commons.lang3.math.NumberUtils;
+
+import java.math.BigDecimal;
+
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        return org.apache.commons.lang3.StringUtils.isNumeric(str);
+       if(NumberUtils.isCreatable(str)){
+           BigDecimal number = NumberUtils.createBigDecimal(str);
+           if (number.compareTo(BigDecimal.ZERO) > 0) {
+               return true;
+           }
+       }
+        return false;
     }
 }
--- src/test/java/com/epam/utils/StringUtilsTest.java
+++ src/test/java/com/epam/utils/StringUtilsTest.java
@@ -1,23 +1,35 @@
 package com.epam.utils;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class StringUtilsTest {
 
-    @Test
-    public void testIsPositiveNumberGiveTrue() {
-        boolean actual = StringUtils.isPositiveNumber("5");
-
-        assertTrue(actual);
+    private static Stream<Arguments> provideStrings() {
+        return Stream.of(
+                Arguments.of("5", true),
+                Arguments.of("", false),
+                Arguments.of("-5", false),
+                Arguments.of("not blank", false),
+                Arguments.of("0", false),
+                Arguments.of("-100000", false),
+                Arguments.of("1000000000000000", true),
+                Arguments.of("10.00000000000000", true),
+                Arguments.of(null, false)
+        );
     }
 
-    @Test
-    public void testIsPositiveNumberGiveFalse() {
-        boolean actual = StringUtils.isPositiveNumber("-5");
+    @ParameterizedTest
+    @MethodSource("provideStrings")
+    public void testIsPositiveNumber(String str, boolean expected) {
+        boolean actual = StringUtils.isPositiveNumber(str);
 
-        assertFalse(actual);
+        assertEquals(expected, actual);
     }
 }
